b34bdefc246d7cb5c62852fa7e1791942c2193c5
Suman Bandita <sumanbandita@gmail.com>
2018-05-21 15:30:44 -0400

    get and get by id

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const data = require('./db/notes');
 
 const app = express();
 
-app.listen(8080,function(){
+app.listen(8081,function(){
   console.info(`Server listening on ${this.address().port}`);  
 }).on('error',err =>{
   console.error(err);
 });
 
+app.get('/api/notes',(req,res)=>{
+  res.json(data);
+});
 
-
+app.get('/api/notes/:id',(req,res)=>{
+  res.json(data.find(item => item.id === Number(req.params.id)));
+});
 
 
 
