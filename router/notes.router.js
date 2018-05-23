f9ef449e21cfa730650b0eca80fec738b29e7469
Suman Bandita <sumanbandita@gmail.com>
2018-05-23 16:17:13 -0400

    move end points to express router

---
+++ router/notes.router.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let express = require('express');
+let router = express.Router();
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+router.get('/notes',(req,res,next)=>{
+  const {searchTerm} = req.query;
+  notes.filter(searchTerm,(err,list) => {
+    if(err){
+      return next(err);
+    }
+    res.json(list);
+  }); 
+});
+
+
+router.get('/notes/:id',(req,res,next)=>{
+  const id = req.params.id;
+  notes.find(id,(err,item)=>{
+    if(err){
+      return next(err);
+    }
+    res.json(item);
+  });  
+});
+
+router.put('/notes/:id',(req,res,next)=>{
+  const id = req.params.id;
+
+  const updateObj = {};
+  const updateField = ['title','content'];
+
+  updateField.forEach(field => {
+    if(field in req.body){
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id,updateObj,(err,item)=>{
+    if(err){
+      return next(err);
+    }
+    if(item){
+      res.json(item);
+    }
+    else{
+      next();
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,61 +2,16 @@
 
 // Load array of notes
 const express = require('express');
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
+
 const {PORT} = require('./config');
 const {logger} = require('./middleware/logger');
 const app = express();
-
+const router = require('./router/notes.router.js');
 app.use(express.static('public'));
 app.use(express.json());//parse request body
 app.use(logger);
 
-app.get('/api/notes',(req,res,next)=>{
-  const {searchTerm} = req.query;
-  notes.filter(searchTerm,(err,list) => {
-    if(err){
-      return next(err);
-    }
-    res.json(list);
-  }); 
-});
-
-app.get('/api/notes/:id',(req,res,next)=>{
-  const id = req.params.id;
-  notes.find(id,(err,item)=>{
-    if(err){
-      return next(err);
-    }
-    res.json(item);
-  });  
-});
-
-app.put('/api/notes/:id',(req,res,next)=>{
-  const id = req.params.id;
-
-  const updateObj = {};
-  const updateField = ['title','content'];
-
-  updateField.forEach(field => {
-    if(field in req.body){
-      updateObj[field] = req.body[field];
-    }
-  });
-    
-  notes.update(id,updateObj,(err,item)=>{
-    if(err){
-      return next(err);
-    }
-    if(item){
-      res.json(item);
-    }
-    else{
-      next();
-    }
-  });
-});
+app.use('/api',router);
 
 //404 handler
 app.use(function(req,res,next){
